3d079166cd5f25ce3eb9be7708939bc07f23e4ae
Mohinthmoki <mohanasundari7067@gmail.com>
2024-06-13 03:32:04 -0700

    file created

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+import ParticlesComponent from './Particles'
+import Login from './Login'
+
+function App() {
+
+
+  return (
+    <>
+    <Login />
+     <ParticlesComponent />
+    </>
+  )
+}
+
+export default App
